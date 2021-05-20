739d2096f70355a1e1119e6f85094fd6ad27fd72
Espiiii-debug <59062281+Espiiii-debug@users.noreply.github.com>
2021-05-20 21:27:02 +0200

    Update Math_Exercice_Python_1.py

--- Math_Exercice_Python_1.py
+++ Math_Exercice_Python_1.py
@@ -1,3 +1,28 @@
+class ProgressBar:
+    '''
+    Progress bar
+    '''
+    def __init__ (self, valmax, maxbar, title):
+        if valmax == 0:  valmax = 1
+        if maxbar > 200: maxbar = 200
+        self.valmax = valmax
+        self.maxbar = maxbar
+        self.title  = title
+    
+    def update(self, val):
+        import sys
+        # format
+        if val > self.valmax: val = self.valmax
+        
+        # process
+        perc  = round((float(val) / float(self.valmax)) * 100)
+        scale = 100.0 / float(self.maxbar)
+        bar   = int(perc / scale)
+  
+        # render 
+        out = '\r %20s [%s%s] %3d %%' % (self.title, '=' * bar, ' ' * (self.maxbar - bar), perc)
+        sys.stdout.write(out)
+
 from math import *
 from random import randint
 
@@ -20,8 +45,11 @@ def calculNombre(min, max, nombreAddition) :
 # Répete le l'opération calculNombre() un nombre de fois defini par l'utilisateur et stock chaque résultat dans une liste.
 def getNumberByLoop(loopNumber, min, max, nombreAddition) :
     result = []
+
+    Bar = ProgressBar(loopNumber, 60, 'Calcul en cours ...')
     for i in range (loopNumber) :
         result.append(calculNombre(min, max, nombreAddition))
+        Bar.update(i)
     
         
     return result
@@ -75,7 +103,8 @@ def main() :
     
     nombreDappartion = checkHowManyNumberRepeatInTheList(b, listNumber)
     
-    print("Le chiffre " + str(b) + " apparait " + str(nombreDappartion) + " fois")
+    print("\nLe chiffre " + str(b) + " apparait " + str(nombreDappartion) + " fois")
+    
 
 # Invoquation de la fonction main()
 main()
